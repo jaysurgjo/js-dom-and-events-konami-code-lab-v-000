d925928bd934ef26163c8550dedb2838ff3ae27c
Jordan Surgick <jordansurgick@aol.com>
2020-05-31 18:03:33 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,16 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  let index = 0; 
 
-let index = 0;
+function init() {
+  document.body.addEventListener('keydown', function(e){
+  const key = parseInt(e.which || e.detail);
+ if (code[index] === key) { index++;
+ if (index === code.length) { alert('Hurray!');
+ index = 0;
+}
+}
+else { index = 0; 
+}
 
-function init(e) {
-
-	document.body.addEventListener('keydown', function(e) {
-		const key = parseInt(e.detail || e.which);
-
-		if (key === code[index]) {
-	    index++;
-
-	    if (index === code.length) {
-	      alert("You're a Konami King!");
-
-	      index = 0;
-	    }
-	  } else {
-	    index = 0;
-	  }
+})}
+init();
