42be81f0958a11013d1711d9420ba82cec294881
Jordan Surgick <jordansurgick@aol.com>
2020-05-31 18:04:35 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   document.body.addEventListener('keydown', function(e){
   const key = parseInt(e.which || e.detail);
  if (code[index] === key) { index++;
- if (index === code.length) { alert('Hurray!');
+ if (index === code.length) {alert('Hurray!');
  index = 0;
 }
 }
